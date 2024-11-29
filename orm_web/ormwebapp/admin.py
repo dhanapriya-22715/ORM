48beba1789af54197597ab3f7aee322cc17d99fd
dhanapriya-22715 <dhanappriyasrinivasan@gmail.com>
2024-11-29 11:40:41 +0530

    Update admin.py

--- orm_web/ormwebapp/admin.py
+++ orm_web/ormwebapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-from .models import Employee,EmployeeAdmin
-admin.site.register(Employee,EmployeeAdmin)
+from .models import Customer, Loan
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number')
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'loan_type', 'loan_amount', 'interest_rate', 'tenure_months', 'issue_date')
